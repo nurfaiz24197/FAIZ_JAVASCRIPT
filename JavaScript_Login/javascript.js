aaf4200d443adcf85562f59732d0b7be71fd0dd7
nurfaiz24197 <114711356+nurfaiz24197@users.noreply.github.com>
2022-10-22 00:18:18 +0800

    Add files via upload

--- JavaScript_Login/javascript.js
+++ JavaScript_Login/javascript.js
@@ -62,7 +62,7 @@ function login(){
     if(a == true && b == true && c == true && d == true && e ==true){
         alert('Tahniah')
     }
-    else if(a == false && b == false && c == false && d == false && e ==false){
+    else (a != true && b != true && c != true && d != true && e !=true){
         /*pass block*/
     }
     else{
